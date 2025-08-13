6a409127cc87dd5074e8ee05a8ea34b06358fb2a
ParaNoob123 <para77710@gmail.com>
2025-08-13 19:47:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-# Start SSH
-service ssh start
 
-# Start XFCE desktop inside VNC session
-export DISPLAY=:0
-startxfce4 &
-
-# Start noVNC
-websockify --web=/usr/share/novnc/ 6080 localhost:5900
